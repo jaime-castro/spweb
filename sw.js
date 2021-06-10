bcdc95012e60eed46d7f2427b4fe300b9e319122
Jaime Castro <jaime.castro5075@gmail.com>
2021-06-10 13:33:55 -0500

    Regresé a tener los tres caches en mi sw

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const STATIC_CACHE = 'static-v1';
+const STATIC_CACHE = 'static-v2';
 const DYNAMIC_CACHE = 'dynamic-v1';
-//const INMUTABLE_CACHE = 'inmutable-v1';
+const INMUTABLE_CACHE = 'inmutable-v2';
 
 // Cache estático
 const APP_SHELL = [
@@ -12,6 +12,10 @@ const APP_SHELL = [
     "assets/NOTICES",
     "assets/AssetManifest.json",
     "assets/FontManifest.json",
+];
+
+// Cache inmutable
+const APP_SHELL_INMUTABLE = [
     "assets/1.png",
     "assets/2.png",
     "assets/3.png",
@@ -75,80 +79,14 @@ const APP_SHELL = [
     "assets/assets/album4.png",
 ];
 
-//Cache inmutable
-//const APP_SHELL_INMUTABLE = [
-//    "assets/1.png",
-//    "assets/2.png",
-//    "assets/3.png",
-//    "assets/4.png",
-//    "assets/5.png",
-//    "assets/6.png",
-//    "assets/7.png",
-//    "assets/8.png",
-//    "assets/9.png",
-//    "assets/10.png",
-//    "assets/11.png",
-//    "assets/12.png",
-//    "assets/13.png",
-//    "assets/14.png",
-//    "assets/15.png",
-//    "assets/16.png",
-//    "assets/17.png",
-//    "assets/18.png",
-//    "assets/19.png",
-//    "assets/20.png",
-//    "assets/21.png",
-//    "assets/22.png",
-//    "assets/23.png",
-//    "assets/24.png",
-//    "assets/25.png",
-//    "assets/26.png",
-//    "assets/27.png",
-//    "assets/28.png",
-//    "assets/29.png",
-//    "assets/30.png",
-//    "assets/31.png",
-//    "assets/32.png",
-//    "assets/33.png",
-//    "assets/34.png",
-//    "assets/35.png",
-//    "assets/36.png",
-//    "assets/37.png",
-//    "assets/38.png",
-//    "assets/39.png",
-//    "assets/40.png",
-//    "assets/41.png",
-//    "assets/42.png",
-//    "assets/43.png",
-//    "assets/44.png",
-//    "assets/45.png",
-//    "assets/46.png",
-//    "assets/47.png",
-//    "assets/48.png",
-//    "assets/49.png",
-//    "assets/50.png",
-//    "assets/51.png",
-//    "assets/52.png",
-//    "assets/53.png",
-//    "assets/song1.wav",
-//    "assets/song2.wav",
-//    "assets/song3.wav",
-//    "assets/song4.wav",
-//    "assets/assets/album1.png",
-//    "assets/assets/album2.png",
-//    "assets/assets/album3.png",
-//    "assets/assets/album4.png",
-//];
-
 self.addEventListener('install', e => {
 
     const cacheStatic = caches.open(STATIC_CACHE).then(cache =>
         cache.addAll(APP_SHELL));
 
-    //const cacheInmutable = caches.open(INMUTABLE_CACHE).then(cache =>
-    //    cache.addAll(APP_SHELL_INMUTABLE));
-
-    //e.waitUntil(Promise.all([cacheStatic, cacheInmutable]));
+    const cacheInmutable = caches.open(INMUTABLE_CACHE).then(cache =>
+        cache.addAll(APP_SHELL_INMUTABLE))
+    e.waitUntil(Promise.all([cacheStatic, cacheInmutable]));
 
     e.waitUntil(Promise.all([cacheStatic]));
 
